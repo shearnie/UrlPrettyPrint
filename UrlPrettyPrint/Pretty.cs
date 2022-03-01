87acc65155113c675aa991dd825a400481ee5b54
Steve Shearn <steve@syntronian.com>
2022-03-01 22:39:29 +1000

    rename, brains in project class

---
+++ UrlPrettyPrint/Pretty.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Web;
+
+namespace UrlPrettyPrint
+{
+    public static class Pretty
+    {
+        public static string Print(string urlValue)
+        {
+
+            if (string.IsNullOrWhiteSpace(urlValue))
+            {
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder();
+
+            // split up
+            var qs = urlValue.Split('?');
+            sb.AppendLine(qs[0]);
+            sb.AppendLine();
+
+            // get params
+            var parms = qs.Count() < 2 ? qs[0] : qs[1];
+            var parse = HttpUtility.ParseQueryString(parms);
+            var keys = (from string k in parse.Keys where !string.IsNullOrEmpty(k) select k).ToList();
+
+            if (!keys.Any())
+            {
+                return string.Empty;
+            }
+
+            // determine spacing
+            var longestLength = keys.OrderByDescending(k => k.Length).First().Length;
+
+            foreach (string k in keys)
+            {
+                sb.Append(k);
+                sb.Append(new String(' ', longestLength - k.Length));
+                sb.Append(" = ");
+                sb.AppendLine(parse[k]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ UrlPrettyPrintWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.Web;
+
+namespace UrlPrettyPrintWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string txturl)
+        {
+            ViewBag.q = txturl;
+            ViewBag.pretty = UrlPrettyPrint.Pretty.Print(txturl);
+            return View("Pretty");
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
